274ae4d93e4c18791b829a2820062c350da13113
jihae0314 <totore1925@naver.com>
2019-08-22 17:30:40 +0900

    Updates

---
+++ precache-manifest.93b7b15148acb22d0e16ecedd76d23c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa801fe261a543c177b0d507111bb5fc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9967244bf5032c6617ab",
+    "url": "/movie_app/static/css/main.8b3a0ce4.chunk.css"
+  },
+  {
+    "revision": "c48c61b86c0853c83a06",
+    "url": "/movie_app/static/js/2.c876b513.chunk.js"
+  },
+  {
+    "revision": "9967244bf5032c6617ab",
+    "url": "/movie_app/static/js/main.ae871bdf.chunk.js"
+  },
+  {
+    "revision": "f9c5d4d9cea5f935b75a",
+    "url": "/movie_app/static/js/runtime~main.b05fbdd8.js"
+  }
+]);
